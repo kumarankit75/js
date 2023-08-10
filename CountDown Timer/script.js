401464825623636a68e3907bc7a05b4b183018d1
scriptedsolutions75 <125038894+scriptedsolutions75@users.noreply.github.com>
2023-08-10 07:09:15 +0530

    Add files via upload

---
+++ CountDown Timer/script.js
@@ -0,0 +1,33 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const mins = document.getElementById('mins');
+const secs = document.getElementById('secs');
+
+const formatTime = (time) => {
+    return time < 10 ? `0${time}`: time; 
+}
+
+const updateCountDown = (deadline) =>{
+    const currentTime = new Date();
+    const timeDifference = deadline - currentTime;//miliseconds
+
+    //calculate days, hours, mins, secs from timeDifference
+    let calSecs = Math.floor(timeDifference/1000)%60;
+    let calcMins = Math.floor(timeDifference/1000/60)%60;
+    let calcHours = Math.floor(timeDifference/1000/60/60)%24;
+    let calcDays = Math.floor(timeDifference /1000 /60 /60 /24);
+
+    days.textContent = formatTime(calcDays);
+    mins.textContent = formatTime(calcMins);
+    hours.textContent = formatTime(calcHours);
+    secs.textContent = formatTime(calSecs);
+    
+
+}
+
+const countDown = (targetDate) => {
+    setInterval(() => updateCountDown(targetDate),1000);
+}
+
+const targetDate = new Date("Aug 15 2023 07:00");
+countDown(targetDate);
